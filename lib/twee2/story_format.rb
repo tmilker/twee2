5c0f0dcf0d65eae687bb9eb69a970980541c8353
Mark Thomas West Harviston <mark.harviston@gmail.com>
2018-06-10 21:38:43 -0700

    set format-version in metadata (only needed for export, but useful in other cases)

--- lib/twee2/story_format.rb
+++ lib/twee2/story_format.rb
@@ -7,19 +7,22 @@ module Twee2
     # Loads the StoryFormat with the specified name
     def initialize(name)
       raise(StoryFormatNotFoundException) if !File::exists?(format_file_path = Twee2::buildpath("storyFormats/#{name}/format.js")) && !File::exists?(format_file_path = "#{name}/format.js")
-      @name = name
       format_file = File::read(format_file_path, encoding: 'utf-8')
       format_data = format_file.match(/(["'])source\1 *: *(["']).*?[^\\]\2/)[0]
       format_data_for_json = "\{#{format_data}\}"
-      @source = JSON.parse(format_data_for_json)['source']
+      json_data = JSON.parse(format_data_for_json)
+      @name    = json_data['name'] || name
+      @version = json_data['version']
+      @source  = json_data['source']
     end
 
     # Given a story file, injects it into the StoryFormat and returns the HTML results
     def compile
       @source \
-        .gsub('{{STORY_NAME}}')   { Twee2::build_config.story_name } \
-        .gsub('{{STORY_DATA}}')   { Twee2::build_config.story_file.xmldata } \
+        .gsub('{{STORY_NAME}}') { Twee2::build_config.story_name } \
+        .gsub('{{STORY_DATA}}') { Twee2::build_config.story_file.xmldata } \
         .gsub('{{STORY_FORMAT}}') { @name }
+        .gsub('{{STORY_FORMAT_VERSION}}') {@version }
     end
 
     # Returns an array containing the known StoryFormat names
